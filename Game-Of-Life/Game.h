f5c52e809dcbfba4db2dc31115d59312d28a4b99
FVCA19 <150895396+FVCA19@users.noreply.github.com>
2024-05-31 23:55:19 +0700

    Add files via upload

---
+++ Game-Of-Life/Game.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <SDL_mixer.h>
+using namespace std;
+
+#include <SDL.h>
+
+struct Cell
+{
+	Cell(bool alive = false) {
+		isAlive = alive;
+	}
+	void Die()  { isAlive = false; }
+	void Born() { isAlive = true; }
+	void Flip() { isAlive = !isAlive; }
+	bool isAlive	 = false;
+	int neighbours   = 0;
+};
+
+struct Grid
+{
+	Grid() {};
+	int rows		 = 0;
+	int cols		 = 0;
+	int posX		 = 0;
+	int posY		 = 0;
+	int width		 = 0;
+	int height		 = 0;
+	float cellWidth  = 0;
+	float cellHeight = 0;
+	int borderSize   = 20;
+};
+
+class Game
+{
+
+public:
+
+	Game();
+	~Game();
+
+	void InitCells(int rows, int cols);
+	void Run();
+	void HandleEvents();
+	void Draw();
+	void AdjustGridSize();
+	int  iX(int i);
+	int	 iY(int i);
+	void RunSimulation();
+private:
+
+	bool isRunning				 = false;
+	bool isRunningSim			 = false;
+	int  simTick				 = 0;
+	int  simDelay				 = 100;
+
+	SDL_Window      *window		 = nullptr;
+
+	SDL_Renderer    *renderer	 = nullptr;
+
+    Mix_Music       *bgMusic   = nullptr;
+
+	int mouseX					 = 0;
+	int mouseY					 = 0;
+	int mouseCellX				 = 0;
+	int mouseCellY				 = 0;
+	bool mouseDown				 = 0;
+
+	Grid grid;
+	vector<vector<Cell>> cells;
+
+};
